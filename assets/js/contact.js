584a956505cdda2e0e92a3dcbf9086b72eada648
Mawanda Henry <mawandahenrynaco@gmail.com>
2019-04-09 18:22:13 +0300

    Version 1.0.1

---
+++ assets/js/contact.js
@@ -0,0 +1,90 @@
+
+
+$(function() {
+
+	//if submit button is clicked
+	$('#submit').click(function () {
+ //var bats = $('#contactform').serialize();
+ //alert(bats);
+		//Get the data from all the fields
+		var name = $('input[name=name]');
+		var email = $('input[name=email]');
+		var regx = /^([a-z0-9_\-\.])+\@([a-z0-9_\-\.])+\.([a-z]{2,4})$/i;
+		var comment = $('textarea[name=message]');
+		var returnError = false;
+  //  alert(name.val()+email.val()+ comment.val())
+    //alert(email);
+		//Simple validation to make sure user entered something
+		//Add your own error checking here with JS, but also do some error checking with PHP.
+		//If error found, add hightlight class to the text field
+		if (name.val()=='') {
+			name.addClass('error');
+			returnError = true;
+		} else name.removeClass('error');
+
+		if (email.val()=='') {
+			email.addClass('error');
+			returnError = true;
+		} else email.removeClass('error');
+
+		if(!regx.test(email.val())){
+          email.addClass('error');
+          returnError = true;
+		} else email.removeClass('error');
+
+
+		if (comment.val()=='') {
+			comment.addClass('error');
+			returnError = true;
+		} else comment.removeClass('error');
+
+		// Highlight all error fields, then quit.
+		if(returnError == true){
+			return false;
+		}
+
+		//organize the data
+
+		var data = 'name=' + name.val() + '&email=' + email.val() + '&comment='  + encodeURIComponent(comment.val());
+
+		//disabled all the text fields
+		$('.text').attr('disabled','true');
+
+		//show the loading sign
+
+
+		//start the ajax
+		$.ajax({
+			//this is the php file that processes the data and sends email
+			url: "contact.php",
+
+			//GET method is used
+			type: "GET",
+
+			//pass the data
+			data: data,
+
+			//Do not cache the page
+			cache: false,
+
+			//success
+			success: function (html) {
+				//if contact.php returned 1/true (send mail success)
+				if (html==1) {
+
+					//show the success message
+						$('.done').fadeIn('slow').delay(2000).hide('slow');
+
+					$(".form").find('input[type=text], textarea').val("");
+          	$('input[name=email]').val("");
+            	$('textarea[name=message]').val("");
+
+				//if contact.php returned 0/false (send mail failed)
+				} else alert('Sorry, unexpected error. Please try again later.');
+			}
+		});
+
+		//cancel the submit button default behaviours
+		return false;
+	});
+});
